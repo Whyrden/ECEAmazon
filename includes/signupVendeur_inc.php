79a1c0e7d91813b701811755c6afe2430f2a4b8b
Whyrden <36502321+Whyrden@users.noreply.github.com>
2019-05-05 15:51:34 +0200

    Add files via upload

--- includes/signupVendeur_inc.php
+++ includes/signupVendeur_inc.php
@@ -10,7 +10,7 @@ if(isset($_POST['signupVendeur_submit'])){
 	$confirm_password=isset($_POST['confirm_password'])? $_POST["confirm_password"]:"";
 	$nom=isset($_POST['nom'])? $_POST["nom"]:"";
 	$prenom=isset($_POST['prenom'])? $_POST["prenom"]:"";
-	$prenom=isset($_POST['description'])? $_POST["description"]:"";
+	$description=isset($_POST['description'])? $_POST["description"]:"";
 
 //Formulaire invalide si...
 	//Un des champs obligatoires est vide
@@ -33,24 +33,41 @@ if(isset($_POST['signupVendeur_submit'])){
 
 //Ajouter à la database si formulaire correct
 	else{
-		$sql="INSERT INTO `vendeurs`(`username_vendeur`,`password`,`email`,`nom`,`prenom`,`description`,`photo_profil`) VALUES ('$identifiant','$password','$mail','$nom','$prenom','description','boy.png')";
+
+		//Si l'username exist, renvoyer à singup.php
+		$sqlCheck="SELECT username_vendeur FROM vendeurs WHERE username_vendeur='$identifiant' ";
+		$stmt=mysqli_stmt_init($db_connect);
+
+			if(!mysqli_stmt_prepare($stmt,$sqlCheck)){
+				header("Location: ../signupvendeur.php?error=sqlerror");
+				exit();
+
+			}
+			else{
+				$resultat=mysqli_query($db_connect, $sqlCheck);
+				if($data=mysqli_fetch_assoc($resultat)){
+					header("Location: ../signupvendeur.php?error=existingUser");
+					exit();
+				}
+			}
+
+		$sql="INSERT INTO `vendeurs`(`username_vendeur`,`password`,`email`,`nom`,`prenom`,`description`,`photo_profil`,`photo_fond`) VALUES ('$identifiant','$password','$mail','$nom','$prenom','$description','boy.png','boy.png')";
+
 		$stmt=mysqli_stmt_init($db_connect);
 
 
 		if(!mysqli_stmt_prepare($stmt,$sql)){
-			header("Location: ../loginVendeur.php?error=sqlerror");
+			header("Location: ../signupVendeur.php?error=sqlerror");
 			exit();
 		}
 		else{
+			mysqli_query($db_connect,$sql);
 
 			//EXecution de la requete
 			if(mysqli_query($db_connect,$sql)){
 			header("Location: ../signupVendeur.php?signupVendeur=success");
 			exit();
 		}
-
-				
-
 		
 		}
 		
